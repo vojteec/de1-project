cbd5ae3b90f6a1a7f4ab45278afcaefa04c274b3
240209 <240209@vutbr.cz>
2023-05-03 22:44:08 +0200

    first testbenches designed

--- tabata_timer/tabata_timer.srcs/sim_1/new/tb_state_to_digits.vhd
+++ tabata_timer/tabata_timer.srcs/sim_1/new/tb_state_to_digits.vhd
@@ -37,7 +37,80 @@ end tb_state_to_digits;
 
 architecture Behavioral of tb_state_to_digits is
 
+	signal sig_lapn   : integer;
+	signal sig_state  : integer;
+	signal sig_dig7   : std_logic_vector(3 downto 0);
+	signal sig_dig6   : std_logic_vector(3 downto 0);
+	signal sig_dig5   : std_logic_vector(3 downto 0);
+	signal sig_dig4   : std_logic_vector(3 downto 0);
+	
 begin
 
+	uut_state_to_digits : entity work.state_to_digits
+		port map (
+			lapn  => sig_lapn,
+			state => sig_state,
+			dig7  => sig_dig7,
+			dig6  => sig_dig6,
+			dig5  => sig_dig5,
+			dig4  => sig_dig4
+		);
 
-end Behavioral;
+	p_lapn : process is
+	begin
+		sig_lapn <= 0;
+		wait for 50 ns;
+		sig_lapn <= 1;
+		wait for 50 ns;
+		sig_lapn <= 2;
+		wait for 50 ns;
+		sig_lapn <= 3;
+		wait for 50 ns;
+		sig_lapn <= 4;
+		wait for 50 ns;
+		sig_lapn <= 5;
+		wait for 50 ns;
+		sig_lapn <= 6;
+		wait for 50 ns;
+		sig_lapn <= 7;
+		wait for 50 ns;
+		sig_lapn <= 8;
+		wait for 50 ns;
+		sig_lapn <= 9;
+		wait for 50 ns;
+		sig_lapn <= 10;
+		wait for 50 ns;
+		sig_lapn <= 11;
+		wait;
+	end process p_lapn;
+	
+	p_state : process is
+	begin
+		sig_state <= 0;
+		wait for 70 ns;
+		sig_state <= 1;
+		wait for 70 ns;
+		sig_state <= 2;
+		wait for 70 ns;
+		sig_state <= 3;
+		wait for 70 ns;
+		sig_state <= 4;
+		wait for 70 ns;
+		sig_state <= 5;
+		wait;
+	end process p_state;
+	
+  --------------------------------------------------------
+  -- Data generation process
+  --------------------------------------------------------
+  p_stimulus : process is
+  begin
+
+    report "Stimulus process started";
+    -- No other input data is needed.
+    report "Stimulus process finished";
+    wait;
+
+  end process p_stimulus;
+  
+end architecture Behavioral;
--- tabata_timer/tabata_timer.srcs/sim_1/new/tb_top.vhd
+++ tabata_timer/tabata_timer.srcs/sim_1/new/tb_top.vhd
@@ -37,13 +37,17 @@ end tb_top;
 
 architecture testbench of tb_top is
 
-signal sig_clk_100mhz   : std_logic;
-signal sig_rst          : std_logic;
-signal sig_up           : std_logic;
-signal sig_down         : std_logic;
-signal sig_left         : std_logic;
-signal sig_right        : std_logic;
-signal sig_switch       : std_logic;
+	constant c_CLK_100MHZ_PERIOD : time := 10 ns;
+	signal sig_clk_100mhz   : std_logic;
+	signal sig_btnc         : std_logic;
+	signal sig_btnu         : std_logic;
+	signal sig_btnd         : std_logic;
+	signal sig_btnl         : std_logic;
+	signal sig_btnr         : std_logic;
+	signal sig_sw           : std_logic;
+	signal sig_cathode      : std_logic_vector(6 downto 0);
+	signal sig_dp			: std_logic;
+	signal sig_anode        : std_logic_vector(7 downto 0);
 
 begin
 
@@ -55,6 +59,149 @@ uut_top : entity work.top
         BTND      => sig_down,
         BTNL      => sig_left,
         BTNR      => sig_right,
-        SW        => sig_switch
+        SW        => sig_switch,
+        CA        => sig_cathode(0),
+        CB        => sig_cathode(1),
+        CC        => sig_cathode(2),
+        CD        => sig_cathode(3),
+        CE        => sig_cathode(4),
+        CF        => sig_cathode(5),
+        CG        => sig_cathode(6),
+        DP        => sig_dp,
+        AN        => sig_anode,
     );
-end testbench;
+	
+  --------------------------------------------------------
+  -- Clock generation process
+  --------------------------------------------------------
+	p_clk_gen : process is
+	begin
+
+		while now < 10000 ns loop -- 10 usec of simulation
+
+		  sig_clk_100mhz <= '0';
+		  wait for c_CLK_100MHZ_PERIOD / 2;
+		  sig_clk_100mhz <= '1';
+		  wait for c_CLK_100MHZ_PERIOD / 2;
+
+		end loop;
+
+		wait;
+	
+	end process p_clk_gen;
+	
+	
+  --------------------------------------------------------
+  -- BTNC generation process
+  --------------------------------------------------------
+  p_btnc_gen : process is
+  begin
+
+	while now < 10000 ns loop -- 10 usec of simulation
+		sig_btnc <= '0';
+		wait for 20 ns;
+
+		sig_btnc <= '1';
+		wait for 50 ns;
+	end loop;
+	
+    wait;
+
+  end process p_btnc_gen;
+  --------------------------------------------------------
+  -- BTNL generation process
+  --------------------------------------------------------
+  p_btnl_gen : process is
+  begin
+
+	while now < 10000 ns loop -- 10 usec of simulation
+		sig_btnl <= '0';
+		wait for 30 ns;
+
+		sig_btnl <= '1';
+		wait for 40 ns;
+	end loop;
+	
+    wait;
+
+  end process p_btnl_gen;
+  --------------------------------------------------------
+  -- BTNR generation process
+  --------------------------------------------------------
+  p_btnr_gen : process is
+  begin
+
+	while now < 10000 ns loop -- 10 usec of simulation
+		sig_btnr <= '0';
+		wait for 35 ns;
+
+		sig_btnr <= '1';
+		wait for 64 ns;
+	end loop;
+
+    wait;
+
+  end process p_btnr_gen;
+  --------------------------------------------------------
+  -- BTNU generation process
+  --------------------------------------------------------
+  p_btnu_gen : process is
+  begin
+
+	while now < 10000 ns loop -- 10 usec of simulation
+		sig_btnu <= '0';
+		wait for 94 ns;
+
+		sig_btnu <= '1';
+		wait for 34 ns;
+	end loop;
+
+    wait;
+
+  end process p_btnu_gen;
+  --------------------------------------------------------
+  -- BTND generation process
+  --------------------------------------------------------
+  p_btnd_gen : process is
+  begin
+
+	while now < 10000 ns loop -- 10 usec of simulation
+		sig_btnd <= '0';
+		wait for 45 ns;
+
+		sig_btnd <= '1';
+		wait for 82 ns;
+	end loop;
+	
+    wait;
+
+  end process p_btnd_gen;
+  --------------------------------------------------------
+  -- SW generation process
+  --------------------------------------------------------
+  p_sw_gen : process is
+  begin
+
+    sig_sw <= '0';
+    wait for 500 ns;
+
+    sig_sw <= '1';
+
+    wait;
+
+  end process p_sw_gen;
+
+  --------------------------------------------------------
+  -- Data generation process
+  --------------------------------------------------------
+  p_stimulus : process is
+  begin
+
+    report "Stimulus process started";
+    -- No other input data is needed.
+    report "Stimulus process finished";
+    wait;
+
+  end process p_stimulus;
+	
+end architecture testbench;
